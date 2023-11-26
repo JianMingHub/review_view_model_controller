90ed657ca561fb80ed49d764cbf299c7ea3200cd
JianMing <lkienminh@gmail.com>
2023-11-26 09:54:40 +0700

    update code data table client side

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -4,9 +4,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>All Laravel - @yield('title')</title>
+  <!-- This is multimenu -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <link href="{{ asset('backend/css/styles.css') }}" rel="stylesheet" />
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  
+  <!-- This is data table -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css" />
+  
+  <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+  <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
 </head>
 <body>
 
@@ -72,6 +80,10 @@
     
   </div>
  
+  <!-- This is multimenu -->
   <script src="{{ asset('backend/js/scripts.js') }}"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  
 </body>
 </html>
--- resources/views/site/category/index.blade.php
+++ resources/views/site/category/index.blade.php
@@ -23,43 +23,56 @@
 @endif
 
 
-<table class="table table-striped">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Name</th>
-        <th scope="col">Status</th>
-        <th scope="col">Delayed</th>
-        <th scope="col">Destination</th>
-        <th scope="col">Slug</th>
-        <th scope="col">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-    @php $count = request()->count && request()->page > 1 ? request()->count : 0; @endphp
+<table id="category" class="table table-striped" style="width:100%">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Name</th>
+      <th>Slug</th>
+      <th>Created by</th>
+      <th>Created date</th>
+      <th >Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    @php $count = 0; @endphp
     @foreach ( $categories as $category)
     @php $count++ @endphp
       <tr>
-        <th scope="row">{{ $count }}</th>
+        <td>{{ $count }}</td>
         <td><a href="{{ route('category.show', ['slugCate' => str_slug($category->name), 'id' => $category->id]) }}">{{ $category->name }}</a></td>
-        <td>{{ $category->status }}</td>
-        <td>{{ $category->delayed }}</td>
-        <td>{{ $category->destination }}</td>
         <td>{{ $category->slug }}</td>
+        <td>{{ $category->slug }}</td>
+        <td>{{ $category->created_at }}</td>
         <td>
-            <a href="{{ route('category.show', ['slugCate' => str_slug($category->name), 'id' => $category->id]) }}" class="btn btn-xs btn-primary" title="Detail">Detail</a>
-            <a href="{{ route('category.edit', $category->id) }}" class="btn btn-xs btn-primary" title="Edit">Edit</a>
-            <a href="{{ route('category.delete', $category->id) }}" class="btn btn-xs btn-danger" title="Detail">Delete</a>
-            @if ($category->status == 1 && $category->destination == 'San Deigo')
-              <a href="{{ route('category.updatedelay', $category->id) }}" class="btn btn-xs btn-danger" title="Update">Delay Update</a>
-            @endif
+          <a href="{{ route('category.show', ['slugCate' => str_slug($category->name), 'id' => $category->id]) }}" class="btn btn-sm btn-primary" title="Detail">Detail</a>
+          <a href="{{ route('category.edit', $category->id) }}" class="btn btn-sm btn-primary" title="Edit">Edit</a>
+          <a href="{{ route('category.delete', $category->id) }}" class="btn btn-sm btn-danger" title="Detail">Delete</a>
+          @if ($category->status == 1 && $category->destination == 'San Deigo')
+            <a href="{{ route('category.updatedelay', $category->id) }}" class="btn btn-sm btn-danger" title="Update">Delay Update</a>
+          @endif
         </td>
       </tr>
     @endforeach
-    </tbody>
-  </table>
-  <div style="display: flex; justify-content: center; padding-bottom: 20px; justify-content: space-between; align-items: center;">
-    <div class="count-categories">{{ $categories->count() }} Categories total</div>
-    {{-- <div class="pagination">{{ $categories->appends(['count' => $count])->links() }}</div> --}}
-  </div>
+  </tbody>
+  <tfoot>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Slug</th>
+        <th>Created by</th>
+        <th>Created date</th>
+        <th >Action</th>
+      </tr>
+  </tfoot>
+</table>
+
+<script>
+  $(document).ready(function() {
+    $('#category').DataTable({
+      "lengthMenu": [5, 10, 25, 50, 100, 500, 1000],
+      "pageLength": 25
+    });
+  });
+</script>
 @endsection
